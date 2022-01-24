37fb5b9bb7a689d44ec08a4b25eec75c62f2bed9
Ragheed Qasmieh <ragheed.qasmieh@mail.mcgill.ca>
2022-01-24 14:15:39 -0500

    Fix naming of exception handling methods

--- src/main/java/com/project/critter/exception/ApiExceptionHandler.java
+++ src/main/java/com/project/critter/exception/ApiExceptionHandler.java
@@ -16,18 +16,18 @@ public class ApiExceptionHandler {
                 new ApiException(e.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now(ZoneId.of("Z"))), HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler(value = EmployeeNotFoundException.class)
-    public ResponseEntity<?> handleCustomerApiRequestException(EmployeeNotFoundException e) {
+    public ResponseEntity<?> handleEmployeeApiRequestException(EmployeeNotFoundException e) {
         return new ResponseEntity<>(
                 new ApiException(e.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now(ZoneId.of("Z"))), HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler(value = PetNotFoundException.class)
-    public ResponseEntity<?> handleCustomerApiRequestException(PetNotFoundException e) {
+    public ResponseEntity<?> handlePetApiRequestException(PetNotFoundException e) {
         return new ResponseEntity<>(
                 new ApiException(e.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now(ZoneId.of("Z"))), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = IllegalStateException.class)
-    public ResponseEntity<?> handleCustomerApiRequestException(IllegalStateException e) {
+    public ResponseEntity<?> handleIllegalStateException(IllegalStateException e) {
         return new ResponseEntity<>(
                 new ApiException(e.getMessage(), HttpStatus.BAD_REQUEST, ZonedDateTime.now(ZoneId.of("Z"))), HttpStatus.BAD_REQUEST);
     }
